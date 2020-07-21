c149a46b3f7463d3b79c0e603850b2721f1c3873
Ibrahim Sharif <shuvoaftab@gmail.com>
2020-07-22 04:25:35 +0600

    lot new

--- Mosh/MySQL.sql
+++ Mosh/MySQL.sql
@@ -422,67 +422,211 @@ WHERE order_id = 2
 ORDER BY (quantity * unit_price) DESC
 
 #
+USE sql_store;
+
+SELECT *
+FROM customers
+LIMIT 3
+
+# /*	Pagination */
+USE sql_store;
+
+SELECT *
+FROM customers
+LIMIT 6, 3
+
+#
+USE sql_store;
+
+SELECT *
+FROM customers
+ORDER BY points DESC
+LIMIT 3
+
+#
+USE sql_store;
+
+SELECT *
+FROM orders
+INNER JOIN customers 
+		ON orders.customer_id = customers.customer_id
+
+#
+USE sql_store;
+
+SELECT order_id, first_name, last_name
+FROM orders
+INNER JOIN customers 
+		ON orders.customer_id = customers.customer_id
+
+#
+USE sql_store;
+
+SELECT order_id, orders.customer_id, first_name, last_name
+FROM orders
+INNER JOIN customers 
+		ON orders.customer_id = customers.customer_id
+
+#
+USE sql_store;
+
+SELECT order_id, o.customer_id, first_name, last_name
+FROM orders o
+INNER JOIN customers C
+		ON o.customer_id = C.customer_id
 
 
 #
+USE sql_store;
+
+SELECT order_id, o.product_id, p.name, quantity, o.unit_price
+FROM order_items o
+JOIN products p
+		ON o.product_id = p.product_id
+
+#	/* Multiple DB	*/
+USE sql_store;
+
+SELECT *
+FROM order_items o
+JOIN sql_inventory.products p
+		ON o.product_id = p.product_id
 
 
 #
+USE sql_inventory;
+
+SELECT *
+FROM sql_store.order_items o
+JOIN products p
+		ON o.product_id = p.product_id
 
 
 #
+USE sql_hr;
+
+SELECT *
+FROM employees e
+JOIN employees m
+		ON e.reports_to = m.employee_id
 
 
 #
+USE sql_hr;
+
+SELECT 
+	e.employee_id,
+    e.first_name,
+    m.first_name AS manager
+FROM employees e
+JOIN employees m
+		ON e.reports_to = m.employee_id
 
 
 #
+USE sql_store;
+
+SELECT *
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id
+JOIN order_statuses os
+	ON o.status = os.order_status_id
 
 
 #
+USE sql_store;
+
+SELECT 	o.order_id,
+		o.order_date,
+        c.first_name,
+        c.last_name,
+        os.name AS status
+FROM orders o
+JOIN customers c
+	ON o.customer_id = c.customer_id
+JOIN order_statuses os
+	ON o.status = os.order_status_id
 
 
 #
+USE sql_invoicing;
+
+SELECT 	p.date,
+		p.invoice_id,
+        p.amount,
+        c.name,
+        pm.name
+FROM payments p
+JOIN clients c
+	ON p.client_id = c.client_id
+JOIN payment_methods pm
+	ON p.payment_method = pm.payment_method_id
 
 
 #
+USE sql_store;
 
-
-#
-
-
-#
-
-
-#
-
+SELECT 	*
+FROM order_items oi
+JOIN order_item_notes oin
+	ON oi.order_id = oin.order_id
+	AND oi.product_id = oin.product_id
 
 #
+USE sql_store;
 
+SELECT 	*
+FROM orders o, customers c
+WHERE o.customer_id = c.customer_id
 
-#
+# /* OUTER is optional */
+USE sql_store;
 
-
-#
-
-
-#
-
-
-#
-
-
-#
-
-
-#
-
+SELECT 	
+		c.customer_id,
+        c.first_name,
+        o.order_id
+FROM customers c
+LEFT JOIN orders o
+	ON c.customer_id = o.customer_id
+ORDER BY c.customer_id
 
 #
+USE sql_store;
 
+SELECT 	
+		c.customer_id,
+        c.first_name,
+        o.order_id
+FROM orders o
+RIGHT JOIN customers c
+	ON c.customer_id = o.customer_id
+ORDER BY c.customer_id
 
 #
+USE sql_store;
 
+SELECT 	
+		p.product_id,
+        p.name,
+        oi.quantity
+FROM  products p
+JOIN order_items oi
+	ON p.product_id = oi.product_id
+ORDER BY p.product_id
+
+#
+USE sql_store;
+
+SELECT 	
+		p.product_id,
+        p.name,
+        oi.quantity
+FROM  products p
+LEFT JOIN order_items oi
+	ON p.product_id = oi.product_id
+ORDER BY p.product_id
 
 #
 
